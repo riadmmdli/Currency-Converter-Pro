29fdf80563869c8b228e1dfb6aacf047382c0698
Riad Mammadli <riadmammadli2003@gmail.com>
2024-08-20 23:43:13 +0400

    Add project files.

---
+++ Currency Converter Pro/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Currency_Converter_Pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fromLabel = new MetroFramework.Controls.MetroLabel();
+            this.toLabel = new MetroFramework.Controls.MetroLabel();
+            this.fromCombo = new MetroFramework.Controls.MetroComboBox();
+            this.toCombo = new MetroFramework.Controls.MetroComboBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.amountTxt = new MetroFramework.Controls.MetroTextBox();
+            this.resultLbl = new MetroFramework.Controls.MetroLabel();
+            this.clearButton = new MetroFramework.Controls.MetroButton();
+            this.changeBtn = new MetroFramework.Controls.MetroButton();
+            this.dateLabel = new MetroFramework.Controls.MetroLabel();
+            this.timeLabel = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // fromLabel
+            // 
+            resources.ApplyResources(this.fromLabel, "fromLabel");
+            this.fromLabel.BackColor = System.Drawing.Color.Teal;
+            this.fromLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.fromLabel.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.fromLabel.Name = "fromLabel";
+            // 
+            // toLabel
+            // 
+            resources.ApplyResources(this.toLabel, "toLabel");
+            this.toLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.toLabel.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.toLabel.Name = "toLabel";
+            this.toLabel.Click += new System.EventHandler(this.toLabel_Click);
+            // 
+            // fromCombo
+            // 
+            this.fromCombo.BackColor = System.Drawing.SystemColors.Control;
+            this.fromCombo.ForeColor = System.Drawing.Color.Red;
+            this.fromCombo.FormattingEnabled = true;
+            resources.ApplyResources(this.fromCombo, "fromCombo");
+            this.fromCombo.Name = "fromCombo";
+            this.fromCombo.Style = MetroFramework.MetroColorStyle.Red;
+            this.fromCombo.UseCustomBackColor = true;
+            this.fromCombo.UseCustomForeColor = true;
+            this.fromCombo.UseSelectable = true;
+            this.fromCombo.UseStyleColors = true;
+            // 
+            // toCombo
+            // 
+            this.toCombo.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.toCombo.ForeColor = System.Drawing.Color.Red;
+            this.toCombo.FormattingEnabled = true;
+            resources.ApplyResources(this.toCombo, "toCombo");
+            this.toCombo.Name = "toCombo";
+            this.toCombo.Style = MetroFramework.MetroColorStyle.Red;
+            this.toCombo.UseCustomBackColor = true;
+            this.toCombo.UseCustomForeColor = true;
+            this.toCombo.UseSelectable = true;
+            this.toCombo.UseStyleColors = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.metroButton1.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.metroButton1.ForeColor = System.Drawing.SystemColors.WindowText;
+            resources.ApplyResources(this.metroButton1, "metroButton1");
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.UseCustomBackColor = true;
+            this.metroButton1.UseCustomForeColor = true;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.UseStyleColors = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroLabel1
+            // 
+            resources.ApplyResources(this.metroLabel1, "metroLabel1");
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.metroLabel1.Name = "metroLabel1";
+            // 
+            // amountTxt
+            // 
+            this.amountTxt.BackColor = System.Drawing.SystemColors.ButtonFace;
+            // 
+            // 
+            // 
+            this.amountTxt.CustomButton.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image")));
+            this.amountTxt.CustomButton.Location = ((System.Drawing.Point)(resources.GetObject("resource.Location")));
+            this.amountTxt.CustomButton.Name = "";
+            this.amountTxt.CustomButton.Size = ((System.Drawing.Size)(resources.GetObject("resource.Size")));
+            this.amountTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.amountTxt.CustomButton.TabIndex = ((int)(resources.GetObject("resource.TabIndex")));
+            this.amountTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.amountTxt.CustomButton.UseSelectable = true;
+            this.amountTxt.CustomButton.Visible = ((bool)(resources.GetObject("resource.Visible")));
+            this.amountTxt.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.amountTxt.FontWeight = MetroFramework.MetroTextBoxWeight.Bold;
+            this.amountTxt.Lines = new string[0];
+            resources.ApplyResources(this.amountTxt, "amountTxt");
+            this.amountTxt.MaxLength = 32767;
+            this.amountTxt.Name = "amountTxt";
+            this.amountTxt.PasswordChar = '\0';
+            this.amountTxt.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.amountTxt.SelectedText = "";
+            this.amountTxt.SelectionLength = 0;
+            this.amountTxt.SelectionStart = 0;
+            this.amountTxt.ShortcutsEnabled = true;
+            this.amountTxt.UseCustomBackColor = true;
+            this.amountTxt.UseCustomForeColor = true;
+            this.amountTxt.UseSelectable = true;
+            this.amountTxt.UseStyleColors = true;
+            this.amountTxt.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.amountTxt.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.amountTxt.Click += new System.EventHandler(this.amountTxt_Click);
+            // 
+            // resultLbl
+            // 
+            resources.ApplyResources(this.resultLbl, "resultLbl");
+            this.resultLbl.BackColor = System.Drawing.Color.White;
+            this.resultLbl.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.resultLbl.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.resultLbl.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Style = MetroFramework.MetroColorStyle.Black;
+            this.resultLbl.UseCustomBackColor = true;
+            this.resultLbl.UseCustomForeColor = true;
+            this.resultLbl.UseStyleColors = true;
+            this.resultLbl.Click += new System.EventHandler(this.resultLbl_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.Red;
+            this.clearButton.FontSize = MetroFramework.MetroButtonSize.Tall;
+            resources.ApplyResources(this.clearButton, "clearButton");
+            this.clearButton.Name = "clearButton";
+            this.clearButton.UseCustomBackColor = true;
+            this.clearButton.UseCustomForeColor = true;
+            this.clearButton.UseSelectable = true;
+            this.clearButton.UseStyleColors = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // changeBtn
+            // 
+            this.changeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.changeBtn.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.changeBtn.ForeColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.changeBtn, "changeBtn");
+            this.changeBtn.Name = "changeBtn";
+            this.changeBtn.UseCustomBackColor = true;
+            this.changeBtn.UseCustomForeColor = true;
+            this.changeBtn.UseSelectable = true;
+            this.changeBtn.UseStyleColors = true;
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            resources.ApplyResources(this.dateLabel, "dateLabel");
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.UseCustomBackColor = true;
+            this.dateLabel.UseCustomForeColor = true;
+            this.dateLabel.UseStyleColors = true;
+            this.dateLabel.Click += new System.EventHandler(this.dateLabel_Click);
+            // 
+            // timeLabel
+            // 
+            resources.ApplyResources(this.timeLabel, "timeLabel");
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.UseCustomBackColor = true;
+            this.timeLabel.UseCustomForeColor = true;
+            this.timeLabel.UseStyleColors = true;
+            this.timeLabel.Click += new System.EventHandler(this.timeLabel_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.changeBtn);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.amountTxt);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.toCombo);
+            this.Controls.Add(this.fromCombo);
+            this.Controls.Add(this.toLabel);
+            this.Controls.Add(this.fromLabel);
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.TransparencyKey = System.Drawing.Color.Empty;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel fromLabel;
+        private MetroFramework.Controls.MetroLabel toLabel;
+        private MetroFramework.Controls.MetroComboBox fromCombo;
+        private MetroFramework.Controls.MetroComboBox toCombo;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox amountTxt;
+        private MetroFramework.Controls.MetroLabel resultLbl;
+        private MetroFramework.Controls.MetroButton clearButton;
+        private MetroFramework.Controls.MetroButton changeBtn;
+        private MetroFramework.Controls.MetroLabel dateLabel;
+        private MetroFramework.Controls.MetroLabel timeLabel;
+    }
+}
+
---
+++ Currency Converter Pro/Form1.cs
@@ -0,0 +1,218 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using MetroFramework.Controls;
+using System.Globalization;
+
+namespace Currency_Converter_Pro
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        
+        
+
+        private Dictionary<string, double> _currencies = new Dictionary<string, double>();
+        public Form1()
+        {
+            InitializeComponent();
+            var currencies = ApiCurrencies();
+            dynamic responseObject = JsonConvert.DeserializeObject(currencies);
+            foreach(var item in responseObject.data)
+            {
+                fromCombo.Items.Add(item.First.code.ToString());
+                toCombo.Items.Add(item.First.code.ToString());
+                _currencies.Add(item.First.code.ToString(), item.First.value.Value);
+            }
+            amountTxt.KeyPress += amountTxt_KeyPress;
+            clearButton.Click += clearButton_Click;
+            changeBtn.Click += changeBtn_Click;
+            // Make the form non-resizable
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            // Disable the maximize button
+            this.MaximizeBox = false;
+
+            // Optionally, set up a timer to update the time every second
+            Timer timer = new Timer();
+            timer.Interval = 1000; // Update every second
+            timer.Tick += (sender, e) => UpdateTimeLabel();
+            timer.Start();
+            timeLabel.AutoSize = true; // Enable auto-sizing
+            timeLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+        }
+       
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            metroButton1.Text = "Convert";
+            metroButton1.BackColor = Color.Teal;
+            metroButton1.ForeColor = Color.White;
+            metroButton1.FlatStyle = FlatStyle.Flat;
+            metroButton1.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+            dateLabel.AutoSize = true;
+            resultLbl.AutoSize = true;
+
+            // Get the current date in English format and language
+            DateTime currentDate = DateTime.Now;
+            string formattedDate = currentDate.ToString("MMMM dd, yyyy", CultureInfo.CreateSpecificCulture("en-US"));
+            dateLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            
+
+            // Set the date label to the formatted date
+            dateLabel.Text = formattedDate;
+            fromCombo.AutoSize = false;
+            toCombo.AutoSize = false;
+
+            // Set the Anchor property to keep the ComboBoxes fixed relative to the top
+            fromCombo.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+            toCombo.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+
+            // Optionally, set the maximum height of the dropdown list
+            fromCombo.DropDownHeight = 200; // Adjust the height as needed
+            toCombo.DropDownHeight = 200;   // Adjust the height as needed
+
+            // Ensure the ComboBoxes do not move up or down
+            fromCombo.Location = new Point(fromCombo.Location.X, 155); // Use the actual Y position
+            toCombo.Location = new Point(toCombo.Location.X, 200);     // Use the actual Y position
+
+            
+        }
+        
+        
+
+        //Here we get currency api key
+        private string ApiCurrencies()
+        {
+            var client = new RestClient("https://api.currencyapi.com/v3/latest?base_currency=USD");
+
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("apikey", "cur_live_TXiKyM8T2Wo5taYrbvNLvGHVQlYD0MhrniTmPuvh");
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+        
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (fromCombo.SelectedItem != null && toCombo.SelectedItem != null && !string.IsNullOrEmpty(amountTxt.Text))
+            {
+                var selectedFromCurrency = fromCombo.SelectedItem.ToString();
+                var selectedToCurrency = toCombo.SelectedItem.ToString();
+                var amount = double.Parse(amountTxt.Text);
+
+                var fromCurrentExRt = _currencies[selectedFromCurrency];
+                var toCurrentExRt = _currencies[selectedToCurrency];
+
+                // Calculate the converted amount
+                var calculatedAmount = (amount / fromCurrentExRt) * toCurrentExRt;
+
+                resultLbl.Text = $"{calculatedAmount:F2} {selectedToCurrency}";
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void toLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the selected items in the combo boxes and the text in the amount textbox
+            fromCombo.SelectedItem = null;
+            toCombo.SelectedItem = null;
+            amountTxt.Text = string.Empty;
+            resultLbl.Text = string.Empty;  // Optionally clear the result label
+        }
+
+        private void amountTxt_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+        private void amountTxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow only digits, control characters (like backspace), and a single decimal point
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true; // Stop the character from being entered into the control
+            }
+
+            // Only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as MetroTextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+            if (char.IsDigit(e.KeyChar) && (sender as MetroTextBox).Text.Replace(".", "").Length >= 7)
+            {
+                e.Handled = true; // Prevent more than 7 digits
+            }
+        }
+
+        private void resultLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void changeBtn_Click(object sender, EventArgs e)
+        {
+            // Store the selected value of fromCombo in a temporary variable
+            var temp = fromCombo.SelectedItem;
+
+            // Swap the selected values
+            fromCombo.SelectedItem = toCombo.SelectedItem;
+            toCombo.SelectedItem = temp;
+        }
+
+        private void dateLabel_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void UpdateTimeLabel()
+        {
+            // Get the current time
+            DateTime currentTime = DateTime.Now;
+
+            // Format the time in 24-hour format
+            string formattedTime = currentTime.ToString("HH:mm:ss", CultureInfo.CreateSpecificCulture("en-US"));
+
+            // Set the formatted time to the label
+            timeLabel.Text = formattedTime;
+        }
+        
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Create a black pen
+            using (Pen blackPen = new Pen(Color.Black, 1))
+            {
+                // Calculate the Y-coordinate for the line
+                int lineY = this.ClientSize.Height - 5; // 5 pixels margin from bottom
+
+
+                // Draw a line at the bottom of the form
+                e.Graphics.DrawLine(blackPen, 0, lineY, this.ClientSize.Width, lineY);
+            }
+        }
+    }
+}
